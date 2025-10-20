c94af26f4830462f564e24851984287895521ad4
llizzza <bondarenkolisa18a@gmail.com>
2025-10-20 08:54:08 +0500

    Update Human.java

--- Human.java
+++ Human.java
@@ -2,23 +2,24 @@ public class Human {
     private String name;
     private int height;
 
-    public void setName(String name) {
+    public Human(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
 
+    public void setName(String name) {
         this.name = name;
     }
 
     public String getName() {
-
         return name;
     }
 
     public void setHeight(int height) {
-
         this.height = height;
     }
 
     public int getHeight() {
-
         return height;
     }
 
